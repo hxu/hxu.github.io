507fbfd5b0782169494f7d0a2dfa763a09d090e0
Han Xu <hgcrpd@gmail.com>
2014-06-25 18:46:56 +0800

    fix path

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,15 +15,18 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
+PATH = 'content'
+
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('You can modify those links in your config file', '#'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('You can add links in your config file', '#'),
+    ('Another social link', '#'),
+)
 
 DEFAULT_PAGINATION = 10
 
